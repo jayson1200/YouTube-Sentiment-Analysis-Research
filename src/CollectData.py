a9926e58a073f0b6444f916517952b275e4b2a61
jayson12000 <jaysonmeribe@gmail.com>
2021-02-12 03:39:49 -0500

    Last commit was bad

--- src/CollectData.py
+++ src/CollectData.py
@@ -23,11 +23,11 @@ def main():
         newValues = []
 
         hour = datetime.now().hour
-        if True:#hour >= 4 and hour <= 20:
+        if hour >= 4 and hour <= 20:
 
             minute = datetime.now().minute
 
-            if True: #minute == 30 or minute == 0:
+            if minute == 30 or minute == 0:
                 newEntry = fillNewEntry(test = False)
 
                 newValues = [newEntry.getSentiment(), newEntry.getMagnitude(), 
