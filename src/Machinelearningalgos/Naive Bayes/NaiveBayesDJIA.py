a977734fa8b73dd6868cdd0a727af56ec16581fd
Jayson <>
2021-04-06 00:52:08 -0400

    stuff

--- src/Machinelearningalgos/Naive Bayes/NaiveBayesDJIA.py
+++ src/Machinelearningalgos/Naive Bayes/NaiveBayesDJIA.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
 
 def mda(y_true, y_pred):
     return np.mean((np.sign(y_true[1:]-y_true[:-1]) == np.sign(y_pred[1:]-y_pred[:-1])).astype(int))
@@ -17,7 +17,7 @@ testingData = data[data["DateTime"]>="2021-03-01 10:00:09"].copy()
 X = np.array(trainingData.drop(["MovemCatSP","DateTime","MovemCatDJIA","MovemCatNAS"], axis = 1).values.tolist())
 y = np.array(trainingData["MovemCatDJIA"].values.tolist())
 
-classifier = RandomForestClassifier()
+classifier = GaussianNB()
 
 classifier.fit(X, y)
 
@@ -27,3 +27,4 @@ y_test = np.array(testingData["MovemCatDJIA"].values.tolist())
 yPred = classifier.predict(X_test)
 
 print(mda(y_test, yPred))
+
--- src/Machinelearningalgos/Naive Bayes/NaiveBayesNASDAQCOMPY.py
+++ src/Machinelearningalgos/Naive Bayes/NaiveBayesNASDAQCOMPY.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
 
 def mda(y_true, y_pred):
     return np.mean((np.sign(y_true[1:]-y_true[:-1]) == np.sign(y_pred[1:]-y_pred[:-1])).astype(int))
@@ -15,15 +15,16 @@ testingData = data[data["DateTime"]>="2021-03-01 10:00:09"].copy()
     
 
 X = np.array(trainingData.drop(["MovemCatSP","DateTime","MovemCatDJIA","MovemCatNAS"], axis = 1).values.tolist())
-y = np.array(trainingData["MovemCatDJIA"].values.tolist())
+y = np.array(trainingData["MovemCatNAS"].values.tolist())
 
-classifier = RandomForestClassifier()
+classifier = GaussianNB()
 
 classifier.fit(X, y)
 
 X_test = np.array(testingData.drop(["MovemCatSP","DateTime","MovemCatDJIA","MovemCatNAS"], axis = 1).values.tolist())
-y_test = np.array(testingData["MovemCatDJIA"].values.tolist())
+y_test = np.array(testingData["MovemCatNAS"].values.tolist())
 
 yPred = classifier.predict(X_test)
 
 print(mda(y_test, yPred))
+
